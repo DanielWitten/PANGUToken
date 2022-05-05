9cbdff21d90cf6fc89ec6c3ce8ac8a5f40439edd
DanielWitten <104780041+DanielWitten@users.noreply.github.com>
2022-05-05 23:38:13 +0800

    Update ERC20.sol

--- ERC20.sol
+++ ERC20.sol
@@ -146,7 +146,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         address from,
         address to,
         uint256 amount
-    ) public external override returns (bool) {
+    ) public external virtual override returns (bool) {
         address spender = _msgSender();
         _spendAllowance(from, spender, amount);
         _transfer(from, to, amount);
